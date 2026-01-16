3e96d82438fb95b7416a59f8d4a9361666c1d491
bookmywashin <bookmywash.in@gmail.com>
2026-01-16 19:37:43 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,3 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const toggle = document.getElementById("menuToggle");
+const nav = document.getElementById("navMenu");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
